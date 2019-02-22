16e4ec35164d0eb0684a45198beb07891dd43bc1
yhiraki <coffexpr@gmail.com>
2019-02-22 13:45:24 +0900

    commit

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build . -t emacs-builder
+docker build . -f Dockerfile-builder -t emacs-builder
 docker run --security-opt seccomp=unconfined --name emacs-builder emacs-builder bash -c "make && make install"
 docker commit $(docker container ps -aq -f name=emacs-builder) emacs-builder:latest
 docker build . -t yhiraki/emacs-minimal
